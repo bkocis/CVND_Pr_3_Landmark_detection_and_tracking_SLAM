14dc2fb97875aa8d998078ace416d31c19d0a576
Balazs Kocsis <balaz.kocis@gmail.com>
2019-08-30 02:14:52 +0200

    initial commit

---
+++ helpers.py
@@ -0,0 +1,290 @@
+from robot_class import robot
+from math import *
+import random
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+# --------
+# this helper function displays the world that a robot is in
+# it assumes the world is a square grid of some given size
+# and that landmarks is a list of landmark positions(an optional argument)
+def display_world(world_size, position, landmarks=None):
+    
+    # using seaborn, set background grid to gray
+    sns.set_style("dark")
+
+    # Plot grid of values
+    world_grid = np.zeros((world_size+1, world_size+1))
+
+    # Set minor axes in between the labels
+    ax=plt.gca()
+    cols = world_size+1
+    rows = world_size+1
+
+    ax.set_xticks([x for x in range(1,cols)],minor=True )
+    ax.set_yticks([y for y in range(1,rows)],minor=True)
+    
+    # Plot grid on minor axes in gray (width = 1)
+    plt.grid(which='minor',ls='-',lw=1, color='white')
+    
+    # Plot grid on major axes in larger width
+    plt.grid(which='major',ls='-',lw=2, color='white')
+    
+    # Create an 'o' character that represents the robot
+    # ha = horizontal alignment, va = vertical
+    ax.text(position[0], position[1], 'o', ha='center', va='center', color='r', fontsize=30)
+    
+    # Draw landmarks if they exists
+    if(landmarks is not None):
+        # loop through all path indices and draw a dot (unless it's at the car's location)
+        for pos in landmarks:
+            if(pos != position):
+                ax.text(pos[0], pos[1], 'x', ha='center', va='center', color='purple', fontsize=20)
+    
+    # Display final result
+    plt.show()
+
+
+    
+def display_world_all_pos(world_size, position, landmarks=None):
+    
+    # using seaborn, set background grid to gray
+    sns.set_style("dark")
+
+    # Plot grid of values
+    world_grid = np.zeros((world_size+1, world_size+1))
+
+    # Set minor axes in between the labels
+    ax=plt.gca()
+    
+    for i in range(1,len(position)):
+        ax.plot([position[i-1][0],position[i][0]], [position[i-1][1],position[i][1]],'r-')
+        
+        ax.plot([position[i][0],position[i][0]], [position[i-1][1],position[i][1]],'b-')
+        ax.plot([position[i-1][0],position[i][0]], [position[i-1][1],position[i-1][1]],'b-')
+    
+    cols = world_size+1
+    rows = world_size+1
+
+    ax.set_xticks([x for x in range(1,cols)],minor=True )
+    ax.set_yticks([y for y in range(1,rows)],minor=True)
+    
+    # Plot grid on minor axes in gray (width = 1)
+    plt.grid(which='minor',ls='-',lw=1, color='white')
+    
+    # Plot grid on major axes in larger width
+    plt.grid(which='major',ls='-',lw=2, color='white')
+    
+    # Create an 'o' character that represents the robot
+    # ha = horizontal alignment, va = vertical
+    for p in position:
+        ax.text(p[0], p[1], 'o', ha='center', va='center', color='r', fontsize=30)
+    
+    ax.text(position[0][0], position[0][1]+2, 'start', ha='center', va='center', color='r', fontsize=30)
+    ax.text(position[-1][0], position[-1][1]+2, 'endpoint', ha='center', va='center', color='r', fontsize=30)
+    
+    #ax.plot([50,60],[50,70],'r-')
+    
+    # Draw landmarks if they exists
+    if(landmarks is not None):
+        # loop through all path indices and draw a dot (unless it's at the car's location)
+        for pos in landmarks:
+            if(pos != position):
+                ax.text(pos[0], pos[1], 'x', ha='center', va='center', color='purple', fontsize=20)
+                
+
+    # Display final result
+    plt.show()
+    
+    
+    
+# --------
+# this routine makes the robot data
+# the data is a list of measurements and movements: [measurements, [dx, dy]]
+# collected over a specified number of time steps, N
+#
+def make_data(N, num_landmarks, world_size, measurement_range, motion_noise, 
+              measurement_noise, distance):
+
+    # check that data has been made
+    try:
+        check_for_data(num_landmarks, world_size, measurement_range, motion_noise, measurement_noise)
+    except ValueError:
+        print('Error: You must implement the sense function in robot_class.py.')
+        return []
+#    print('Check done')
+    complete = False
+    
+    r = robot(world_size, measurement_range, motion_noise, measurement_noise)
+    r.make_landmarks(num_landmarks)
+    counter=0
+    while not complete:
+
+        data = []
+
+        seen = [False for row in range(num_landmarks)]
+    
+        # guess an initial motion
+        orientation = random.random() * 2.0 * pi
+        dx = cos(orientation) * distance
+        dy = sin(orientation) * distance
+            
+        for k in range(N-1):
+    
+            # collect sensor measurements in a list, Z
+            Z = r.sense()
+#            print(Z)
+            # check off all landmarks that were observed 
+            for i in range(len(Z)):
+                seen[Z[i][0]] = True
+    
+            # move
+            while not r.move(dx, dy):
+                # if we'd be leaving the robot world, pick instead a new direction
+                orientation = random.random() * 2.0 * pi
+                dx = cos(orientation) * distance
+                dy = sin(orientation) * distance
+
+            # collect/memorize all sensor and motion data
+            data.append([Z, [dx, dy]])
+
+        # we are done when all landmarks were observed; otherwise re-run
+        complete = (sum(seen) == num_landmarks)
+        counter+=1
+#        print(counter, seen)
+    print(' ')
+    print('Landmarks: ', r.landmarks)
+    print(r)
+
+
+    return data
+
+
+def make_data_2(N, num_landmarks, world_size, measurement_range, motion_noise, 
+              measurement_noise, distance):
+
+    # check that data has been made
+    try:
+        check_for_data(num_landmarks, world_size, measurement_range, motion_noise, measurement_noise)
+    except ValueError:
+        print('Error: You must implement the sense function in robot_class.py.')
+        return []
+#    print('Check done')
+    complete = False
+    
+    r = robot(world_size, measurement_range, motion_noise, measurement_noise)
+    r.make_landmarks(num_landmarks)
+    counter=0
+    while not complete:
+
+        data = []
+
+        seen = [False for row in range(num_landmarks)]
+    
+        # guess an initial motion
+        orientation = random.random() * 2.0 * pi
+        dx = cos(orientation) * distance
+        dy = sin(orientation) * distance
+            
+        for k in range(N-1):
+    
+            # collect sensor measurements in a list, Z
+            Z = r.sense()
+#            print(Z)
+            # check off all landmarks that were observed 
+            for i in range(len(Z)):
+                seen[Z[i][0]] = True
+    
+            # move
+            while not r.move(dx, dy):
+                # if we'd be leaving the robot world, pick instead a new direction
+                orientation = random.random() * 2.0 * pi
+                dx = cos(orientation) * distance
+                dy = sin(orientation) * distance
+
+            # collect/memorize all sensor and motion data
+            data.append([Z, [dx, dy]])
+
+        # we are done when all landmarks were observed; otherwise re-run
+        complete = (sum(seen) == num_landmarks)
+        counter+=1
+#        print(counter, seen)
+#    print(' ')
+#    print('Landmarks: ', r.landmarks)
+#    print(r)
+
+
+    return data, r.landmarks
+
+
+def make_data_3(N, num_landmarks, world_size, measurement_range, motion_noise, 
+              measurement_noise, distance):
+
+    # check that data has been made
+    try:
+        check_for_data(num_landmarks, world_size, measurement_range, motion_noise, measurement_noise)
+    except ValueError:
+        print('Error: You must implement the sense function in robot_class.py.')
+        return []
+#    print('Check done')
+    complete = False
+    
+    r = robot(world_size, measurement_range, motion_noise, measurement_noise)
+    r.make_landmarks_non_rand(num_landmarks)
+    counter=0
+    while not complete:
+
+        data = []
+
+        seen = [False for row in range(num_landmarks)]
+    
+        # guess an initial motion
+        orientation = random.random() * 2.0 * pi
+        dx = cos(orientation) * distance
+        dy = sin(orientation) * distance
+            
+        for k in range(N-1):
+    
+            # collect sensor measurements in a list, Z
+            Z = r.sense()
+#            print(Z)
+            # check off all landmarks that were observed 
+            for i in range(len(Z)):
+                seen[Z[i][0]] = True
+    
+            # move
+            while not r.move(dx, dy):
+                # if we'd be leaving the robot world, pick instead a new direction
+                orientation = random.random() * 2.0 * pi
+                dx = cos(orientation) * distance
+                dy = sin(orientation) * distance
+
+            # collect/memorize all sensor and motion data
+            data.append([Z, [dx, dy]])
+
+        # we are done when all landmarks were observed; otherwise re-run
+        complete = (sum(seen) == num_landmarks)
+        counter+=1
+#        print(counter, seen)
+    print(' ')
+    print('Landmarks: ', r.landmarks)
+    print(r)
+
+
+    return data
+
+
+
+
+def check_for_data(num_landmarks, world_size, measurement_range, motion_noise, measurement_noise):
+    # make robot and landmarks
+    r = robot(world_size, measurement_range, motion_noise, measurement_noise)
+    r.make_landmarks(num_landmarks)
+#    print('....')
+    
+    # check that sense has been implemented/data has been made
+    test_Z = r.sense()
+    if(test_Z is None):
+        raise ValueError
